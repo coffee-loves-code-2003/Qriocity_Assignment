48608cd80247bb570420cf790136d277532f1da1
coffee-loves-code-2003 <128959041+coffee-loves-code-2003@users.noreply.github.com>
2024-05-19 22:36:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENVIRONMENT==="production")
         app.use(express.static(path.join(__dirname,'../frontend/build')));
         app.get('*',(req,res)=>
         {
-            res.sendFile(path.resolve(__dirname,'../build/index.html'))
+            res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
         })
     }
 
